cba89778917f7f79c41bf6953f69eeca2d4f656e
Daniel Maciel Acevedo <anndannma@outlook.com>
2022-09-24 19:27:08 -0500

    Se creó el digitalClock con getHours,getMinutes and getSeconds

---
+++ index.js
@@ -0,0 +1,18 @@
+const currentTime = () => {
+    const clock = document.getElementById("digital-clock");
+
+    let date = new Date();
+    let hh = date.getHours();
+    let mm = date.getMinutes();
+    let ss = date.getSeconds();
+
+    hh = hh < 10 ? `0${hh}` : hh;
+    mm = mm < 10 ? `0${mm}` : mm;
+    ss = ss < 10 ? `0${ss}` : ss;
+    
+    let time = `${hh}:${mm}:${ss}`;
+    clock.innerText = time;
+}
+
+currentTime();
+setInterval(currentTime, 1000);
